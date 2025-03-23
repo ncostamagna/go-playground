37d78582edb368898668dbc916ebc7d86cf0693d
Nahue <nlcostamagna@gmail.com>
2025-03-23 15:51:50 -0300

    changes

--- rune/main.go
+++ rune/main.go
@@ -2,7 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
+
+type textAnalyzer struct {
+	wordCount int
+	lineCount int
+	characterCount int
+	upperCount int
+	lowerCount int
+}
+
+func NewTextAnalyzer() *textAnalyzer {
+	return &textAnalyzer{}
+}
+
+func (t *textAnalyzer) Analyze(text string) {
+	//  count the number of runes in the string
+	t.lineCount = utf8.RuneCountInString(text) - utf8.RuneCountInString(strings.ReplaceAll(text, "\n", "")) //count the lines in our text
+	t.characterCount = utf8.RuneCountInString(text)
+
+	t.wordCount = len(strings.Fields(text)) // count the words in our text
+	for _, r := range text {
+		if unicode.IsUpper(r) {
+			t.upperCount++
+		} else if unicode.IsLower(r) {
+			t.lowerCount++
+		}
+	}
+}
+
+func (t textAnalyzer) PrintsStats() {
+	fmt.Printf("Word count: %d\n", t.wordCount)
+	fmt.Printf("Line count: %d\n", t.lineCount)
+	fmt.Printf("Character count: %d\n", t.characterCount)
+	fmt.Printf("Upper count: %d\n", t.upperCount)
+	fmt.Printf("Lower count: %d\n", t.lowerCount)
+}
 
 func main() {
 	// Define a string
@@ -23,10 +61,18 @@ func main() {
 	fmt.Println("\nString to Runes:")
 	runes := []rune(str)
 	for i, r := range runes {
-		fmt.Printf("Index: %d, Rune: '%c', Unicode: U+%04X\n", i, r, r)
+		fmt.Printf("Index: %d, Rune: '%c' - %d, Unicode: U+%04X\n", i, r, r, r)
 	}
 
 	// Length of string vs length of runes
 	fmt.Printf("\nString length (bytes): %d\n", len(str))
 	fmt.Printf("Number of runes: %d\n", len(runes))
+	fmt.Println()
+	{
+		text := `Hello 世
+						my friend ã`
+		a := NewTextAnalyzer()
+		a.Analyze(text)
+		a.PrintsStats()
+}
 }
---
+++ rune/readme.md
@@ -0,0 +1,5 @@
+
+- each character in Golang is called **rune**. 
+- rune is an alias of int32.
+- Its so important when you dont have a english text or non-ASCII text, like emonijc or chinies languages.
+- we use multibyte characters depends on if we use a or ǎ
